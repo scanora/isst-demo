d5581be4ae7df8cd1b1bc97eca590a074235263a
Santiago Canora Fernández <s.canora@alumnos.upm.es>
2023-04-03 21:53:27 +0200

    inicial
    primer proyecto que subo

---
+++ is-the-site-up/src/test/java/demo/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
